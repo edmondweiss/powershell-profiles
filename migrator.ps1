432f68db6e82e23bf73c564834e77758d3a6dd41
Edmond Weiss <edmondweiss@gmail.com>
2019-01-27 01:10:06 -0500

    Add script to automate the movement process of a profile
    The profile file will have to be moved to an appropriate location in the computer to take effect. The script automates the process of moving the script.

---
+++ migrator.ps1
@@ -0,0 +1,35 @@
+param(
+  [Parameter(Mandatory = $true)]
+  [String]
+  $ScriptName,
+
+  [Parameter()]
+  [String]
+  $Destination = $PROFILE.currentUserAllHosts,
+
+  [Parameter()]
+  [String]
+  $NewScriptName = 'profile.ps1'
+)
+
+$hasDestinationPath = Test-Path $Destination -PathType Container
+if (!$hasDestinationPath) {
+  Write-Host "The destination path $Destination is invalid."
+  exit
+}
+
+$ScriptName = Split-Path $ScriptName -Leaf
+$scriptLocation = "$PSScriptRoot\$ScriptName"
+$hasScript = Test-Path $scriptLocation
+if (!$hasScript) {
+  Write-Host "The script ($ScriptName) does not exist at $PSScriptRoot."
+  exit
+}
+
+$scriptCopy = Copy-Item $scriptLocation "$PSScriptRoot\$NewScriptName" -PassThru
+$Destination = Resolve-Path $Destination
+if ($Destination -ne $scriptCopy.DirectoryName) {
+  Remove-Item -Path "$Destination\$NewScriptName" -ErrorAction SilentlyContinue
+}
+
+Move-Item -Path $scriptCopy -Destination $Destination
