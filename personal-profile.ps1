20770266e9ab04c6e87ec2923b684731ec2ce0b1
Edmond Weiss <edmondweiss@gmail.com>
2019-01-27 01:06:58 -0500

    Add profile used for my personal computer

---
+++ personal-profile.ps1
@@ -0,0 +1,37 @@
+Set-Variable -Name HOME -Value 'C:\edmond' -Force
+
+Set-Location $HOME
+
+Set-Alias -Name gh -Value Get-Help -Option AllScope
+Set-Alias -Name uh -Value Update-Help -Option AllScope
+Set-Alias -Name rdir -Value Remove-Directory -Option AllScope
+Set-Alias -Name ndir -Value New-Directory -Option AllScope
+Set-Alias -Name home -Value Set-Home -Option AllScope
+
+function Remove-Directory {
+  Param(
+    [Parameter(
+      Mandatory = $true,
+      ValueFromPipelineByPropertyName = $true
+    )]
+    [String]
+    $Path
+  )
+  Remove-Item -Path $Path -Recurse -Force
+}
+
+function New-Directory {
+  Param(
+    [Parameter(
+      Mandatory = $true,
+      ValueFromPipelineByPropertyName = $true
+    )]
+    [String]
+    $Name
+  )
+  New-Item -Name $Name -ItemType Directory
+}
+
+function Set-Home {
+  Set-Location $HOME
+}
